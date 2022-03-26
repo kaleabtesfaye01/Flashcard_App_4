ede996266ac008614e10b6a0d4f4595bf6dd539e
kaleabtesfaye01 <kaleabtesfaye01@gmail.com>
2022-03-26 14:05:06 -0400

    update

--- app/src/main/java/com/example/flashcardapp2/MainActivity.java
+++ app/src/main/java/com/example/flashcardapp2/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.flashcardapp2;
 
 import android.content.Intent;
-import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
